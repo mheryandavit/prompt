85ebe466b609218a34e78948dcf8b1e32494c387
Davit <mheryandavit08@gmail.com>
2023-04-24 21:21:07 +0400

    second prompt

--- script.js
+++ script.js
@@ -7,13 +7,33 @@ const personalMovieDB={
     privat:false
 };
 
-let a=prompt("Один из последних фильмов?");
-let b=prompt("на сколько оцените его?");
-let c=prompt("Один из последних фильмов?");
-let d=prompt("на сколько оцените его?");
+
+// let c=prompt("Один из последних фильмов?");
+// let d=prompt("на сколько оцените его?");
 
 
-personalMovieDB.movies[a]=b
-personalMovieDB.movies[c9]=d
 
+// personalMovieDB.movies[c9]=d
+
+for(let i=0;i<2;i++){
+    let a=prompt("Один из последних фильмов?");
+    let b=prompt("на сколько оцените его?");
+  
+    if(a !=null && b!==null && a!=""&& b!="" && a.length<50){
+        personalMovieDB.movies[a]=b
+        console.log("dane")
+    }else{
+        console.log("error")
+        i--;
+    }
+}
+if(personalMovieDB.count<10){
+    console.log("Просмотрено довольно мало фильмов")
+}else if(personalMovieDB.count>10 && personalMovieDB.count<30 ){
+    console.log("вы классный зритель")
+}else if(personalMovieDB.count>30){
+    console.log(" Киноман")
+}else{
+    console.log("произошла ошибка")
+}
 console.log(personalMovieDB)
